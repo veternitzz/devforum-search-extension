53119c7d1d4c49ae3091dac2da484e2a89325b05
vet <anthonylapeter@gmail.com>
2024-07-23 14:08:01 -0400

    Made the commands and made them work

--- extension.js
+++ extension.js
@@ -4,12 +4,27 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log('Congratulations, your extension "devforum-search" is now active!');
+	// lets register our search command
+	const disposable = vscode.commands.registerCommand('devforum-search.search-devforum', async function () {
+		// capture the input from the user so we know what to search for
+		const input = await vscode.window.showInputBox({placeHolder: 'Search the devforum'});
 
-	const disposable = vscode.commands.registerCommand('devforum-search.search-devforum', function () {
+		// if the user left an empty string then return
+		if (input === ''){return}
 
-		vscode.window.showInformationMessage('Hello World from Devforum Search!');
+		// if the user didn't hit esc
+		if (input !== undefined){
+			const url = `https://devforum.roblox.com/search?expanded=false&q=${input}` // define url
+			vscode.window.showInformationMessage("Opening in an external browser") // let the user know we're trying to open in an external browser
+			vscode.env.openExternal(vscode.Uri.parse(url)) // open in an external browser
+		}
 	});
+
+	// lets now register a command which sends them to the github repo
+	const disposable2 = vscode.commands.registerCommand('devforum-search.view-github', function(){
+		const url = `https://github.com/veternitzz/devforum-search-extension` // define url
+		vscode.env.openExternal(url) // open in an external browser
+	})
 
 	context.subscriptions.push(disposable);
 }
